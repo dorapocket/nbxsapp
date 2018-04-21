38fcc928b6ff4e10f8f41ddd768b3eba1dec7261
dorapocket <993549790@qq.com>
2018-04-21 10:42:57 +0800

    支持boardnews readnews全解析
    支持boardnews readnews全解析

--- boardnews.php
+++ boardnews.php
@@ -1,41 +1,33 @@
-﻿<head>
-<meta http-equiv="content-type" content="text/html;charset=utf-8">
-</head>
-<body>
 <?php
-include('simple_html_dom.php');
 $ch = curl_init();
 $timeout = 5;
-$title="meiyou";
+$title=null;
 $id=$_GET["id"];
-$targeturl='http://www.nbxiaoshi.net/ReadNews.asp?NewsId='.$id;
+$targeturl='http://www.nbxiaoshi.net/Board_news.asp?ID='.$id;
 curl_setopt ($ch, CURLOPT_URL, $targeturl);
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $a =curl_exec($ch);
 curl_close($ch);
-$file_contents=yang_gbk2utf8($a);
+$file_contents=gbk2utf8($a);
 #echo $file_contents;
 $pa=0;
 $html = new simple_html_dom();
 $html->load($file_contents);
 $i=0;
 $itemcount=1;
-
 foreach($html->find('strong') as $tit) {
-     $title=$tit->innertext;}
-if($title=="meiyou"){
-include "boardnews.php";
-}else{
+     $title=$tit->innertext;
+}
 foreach($html->find('td[id=fontzoom]') as $para) {
      $paragraph=$para->innertext;
 }
 
 echo 'title:'.$title;
 echo str_replace("/ewebeditor","http://www.nbxiaoshi.net/ewebeditor",$paragraph);
-}
 
-function yang_gbk2utf8($str){ 
+
+function gbk2utf8($str){ 
     $charset = mb_detect_encoding($str,array('UTF-8','GBK','GB2312')); 
     $charset = strtolower($charset); 
     if('cp936' == $charset){ 
@@ -47,4 +39,3 @@ function yang_gbk2utf8($str){
     return $str; 
 }
 ?>
-</body>
